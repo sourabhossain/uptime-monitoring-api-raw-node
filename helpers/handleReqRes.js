bd0fcbf12bfc46a86679d93b9f2e1b215c9f9573
Sourab Hossain <sourabhossain.cse@gmail.com>
2021-04-09 04:39:44 +0600

    handle module add

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,40 @@
+/**
+ * Title: Handle Request Response
+ * Description: Handle Request and Response
+ * Author: Sourab Hossain
+ * Date: 09-04-2021
+ */
+
+// dependencies
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
+// module scaffolding
+const handler = {};
+
+handler.handleReqRes = (req, res) => {
+    // request handling
+    // get the url and parse it
+    const parseUrl = url.parse(req.url, true);
+    const path = parseUrl.path;
+    const trimemedPath = path.replace(/^\/+|\+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parseUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello World!');
+    });
+};
+
+module.exports = handler;
--- index.js
+++ index.js
@@ -6,29 +6,28 @@
  */
 
 // dependencies
-const http = require("http");
+const http = require('http');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-  port: 3000,
+    port: 3000,
 };
 
 // create server
 app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`listening to port ${app.config.port}`);
-  });
+    const server = http.createServer(app.handleReqRes);
+
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`);
+    });
 };
 
 // handle request response
-app.handleReqRes = (req, res) => {
-  // response handle
-  res.end("Hello World!");
-};
+app.handleReqRes = handleReqRes;
 
 // start the server
 app.createServer();
