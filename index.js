c35d1924412af9e09c4cfb2044314eb4d0e493df
Sourab Hossain <sourabhossain.cse@gmail.com>
2021-04-10 15:14:15 +0600

    data insert, read, update, delete form file

--- index.js
+++ index.js
@@ -9,9 +9,35 @@
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
+
+// testing file system
+// TODO: test done then remove test code
+
+/**
+ * -- DATA INSERT --
+ * data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
+ *     console.log(`error was`, err);
+ * });
+ *
+ * -- DATA READ --
+ * data.read('test', 'newFile', (err, result) => {
+ *    console.log(err, result);
+ * });
+ *
+ * -- Data Update --
+ * data.update('test', 'newFile', {'name': 'Bangladesh', 'language': 'Bangla'}, (err) => {
+ *  console.log(err);
+ * });
+ *
+ * -- Data Delete --
+ * data.delete('test', 'newFile', (err) => {
+ *   console.log(err);
+ * });
+ */
 
 // create server
 app.createServer = () => {
